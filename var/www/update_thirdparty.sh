67d30b6936687115f1bdcade17ff220cecfc72bf
Gerard Wagener <gerard.wagener@circl.lu>
2015-03-06 13:57:27 +0100

    Updated update_thirdparty.sh installation script

--- var/www/update_thirdparty.sh
+++ var/www/update_thirdparty.sh
@@ -10,9 +10,9 @@ filename="sb-admin-${SBADMIN_VERSION}"
 rm -rf temp
 mkdir temp
 
-wget http://startbootstrap.com/downloads/${filename}".zip" -O temp/${filename}".zip"
-
+wget https://github.com/IronSummitMedia/startbootstrap-sb-admin-2/archive/v1.0.2.zip -O temp/${filename}".zip"
 unzip temp/${filename}".zip" -d temp/
+mv temp/startbootstrap-sb-admin-2-1.0.2 temp/sb-admin-2
 
 JQVERSION="1.11.1"
 wget http://code.jquery.com/jquery-${JQVERSION}.js -O ./static/js/jquery.js
@@ -22,17 +22,15 @@ wget https://collabdev.googlecode.com/svn-history/r5/trunk/static/js/jquery.time
 #Here to fix an error about an hard dependency in a obscur script of bootstrap..
 wget http://code.jquery.com/jquery-1.4.2.js -O ./static/js/jquery-1.4.2.js
 
+wget http://www.goat1000.com/jquery.tagcanvas.js?2.5 -O ./static/js/jquery.tagcanvas.js
+
 rm -rf ./static/js/plugins
 mv temp/${filename}/js/* ./static/js/
 
 rm -rf ./static/fonts/ ./static/font-awesome-4.1.0/
 
 mv temp/${filename}/fonts/ ./static/
-mv temp/${filename}/font-awesome-4.1.0/ ./static/
+mv temp/${filename}/font-awesome/ ./static/
 
 rm -rf ./static/css/plugins/
 mv temp/${filename}/css/* ./static/css/
-
-rm -rf temp/
-
-wget https://raw.githubusercontent.com/eternicode/bootstrap-datepicker/master/js/bootstrap-datepicker.js -O ./static/js/bootstrap-datepicker.js
