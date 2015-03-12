5847f9fd30a7daf04cf4fd137c92debac51c1aa5
Gerard Wagener <gerard.wagener@circl.lu>
2015-03-12 09:51:18 +0100

    Fixed double dictionary bug

--- bin/PotironAnPDNS.py
+++ bin/PotironAnPDNS.py
@@ -53,10 +53,10 @@ class AnnotatePDNS(Annotate):
         return (self.cacheid, r)
 
     def annoate_doc(self, doc):
-        (rid,name) = self.get_rrnames(doc["ipsrc"])
         if doc['state'] & potiron.STATE_PDNS_AN:
             #The document was already annotated
             return doc
+        (rid,name) = self.get_rrnames(doc["ipsrc"])
         if name != "":
             doc["a_"+str(potiron.TYPE_PDNS_DICT)+"_ipsrc_pdns"] = rid
         (rid,name) = self.get_rrnames(doc["ipdst"])
