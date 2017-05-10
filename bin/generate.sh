ec9e1992a1f7a692b1477a59d17d8ace483f7f88
chrisr3d <chris.studer.68@gmail.com>
2017-05-10 10:26:32 +0200

    Logo file can be defined by the user in the "most frequent occurences of a field" chart
    The default file is also the one used by default in the bokeh plot and stored in the doc/ directory of potiron

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for file in $1*.csv ; do
 	name=$( echo ${file} | rev | cut -f1 -d/ | rev | cut -f1 -d.)
-	echo ${name}
-	cat "template.html" | sed -e "s/##NAME##/${name}/g" >$1/${name}.html
+	if [ -z "$2" ]
+	then
+		logo="$(dirname "$(pwd)")/doc/circl.png"
+	else
+		logo=$2
+	fi
+	cat "template.html" | sed -e "s/##NAME##/${name}/g" | sed -e "s_##LOGO##_${logo}_g" >$1/${name}.html
 done
 exit 0
