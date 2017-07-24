c15b87b75a973a04bc8412f909c555d6e53bb705
chrisr3d <chris.studer.68@gmail.com>
2017-07-24 09:35:06 +0200

    Got rid of reassignment of the logo file variable

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+if [ -z "$2" ]
+then
+	logo="$(dirname "$(pwd)")/doc/circl.png"
+else
+	logo=$2
+fi
 for file in $( ls $1*.csv | grep -v parallel-coordinate)  ; do
 	name=$( echo ${file} | rev | cut -f1 -d/ | rev | cut -f1 -d.)
-	if [ -z "$2" ]
-	then
-		logo="$(dirname "$(pwd)")/doc/circl.png"
-	else
-		logo=$2
-	fi
 	cat "template.html" | sed -e "s/##NAME##/${name}/g" | sed -e "s_##LOGO##_${logo}_g" >$1/${name}.html
 done
 exit 0
