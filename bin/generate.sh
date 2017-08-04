b0b76d37f14f2d5f8b389c7237e67b6eac28d8b7
chrisr3d <chris.studer.68@gmail.com>
2017-08-04 10:40:37 +0200

    Error fixed

--- bin/generate.sh
+++ bin/generate.sh
@@ -5,7 +5,7 @@ then
 else
 	logo=$2
 fi
-for file in $( ls $1*.csv | grep -v parallel-coordinate)  ; do
+for file in $( ls $1*.csv)  ; do
 	name=$( echo ${file} | rev | cut -f1 -d/ | rev | cut -f1 -d.)
 	cat "template.html" | sed -e "s/##NAME##/${name}/g" | sed -e "s_##LOGO##_${logo}_g" >$1/${name}.html
 done
