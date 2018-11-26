8c23dc0f88bd59dcf7e273958d0db651a582461f
D4-project dev <dev@d4-project.org>
2018-11-26 17:01:16 +0100

    fix: Fixed js libraries installation

--- var/www/update_thirdparty.sh
+++ var/www/update_thirdparty.sh
@@ -10,14 +10,14 @@ filename="sb-admin-${SBADMIN_VERSION}"
 rm -rf temp
 mkdir temp
 
-wget https://github.com/IronSummitMedia/startbootstrap-sb-admin-2/archive/v1.0.2.zip -O temp/${filename}".zip"
+wget https://github.com/BlackrockDigital/startbootstrap-sb-admin-2/archive/gh-pages.zip -O temp/${filename}".zip"
 unzip temp/${filename}".zip" -d temp/
-mv temp/startbootstrap-sb-admin-2-1.0.2 temp/sb-admin-2
+mv temp/startbootstrap-sb-admin-2-gh-pages temp/sb-admin-2
 
 JQVERSION="1.11.1"
 wget http://code.jquery.com/jquery-${JQVERSION}.js -O ./static/js/jquery.js
 
-wget https://collabdev.googlecode.com/svn-history/r5/trunk/static/js/jquery.timers-1.0.0.js -O ./static/js/jquery.timers-1.0.0.js
+wget https://osdn.net/projects/sfnet_tmpackage/downloads/taskmanager%200.9/0.9/ZopeProduct/ZPTaskManager/js/jquery.timers-1.0.0.js -O ./static/js/jquery.timers-1.0.0.js
 
 #Here to fix an error about an hard dependency in a obscur script of bootstrap..
 wget http://code.jquery.com/jquery-1.4.2.js -O ./static/js/jquery-1.4.2.js
@@ -29,11 +29,10 @@ mv temp/${filename}/js/* ./static/js/
 
 rm -rf ./static/fonts/ ./static/font-awesome-4.1.0/
 
-mv temp/${filename}/fonts/ ./static/
-mv temp/${filename}/font-awesome/ ./static/
+#mv temp/${filename}/fonts/ ./static/
+#mv temp/${filename}/font-awesome/ ./static/
 
 rm -rf ./static/css/plugins/
-mv temp/${filename}/css/* ./static/css/
+mv temp/${filename}/dist/css/* ./static/css/
 
 wget https://raw.githubusercontent.com/eternicode/bootstrap-datepicker/master/js/bootstrap-datepicker.js -O ./static/js/bootstrap-datepicker.js
-
