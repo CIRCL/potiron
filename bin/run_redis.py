da05b4515ad129b30bee8e7aaa516e3e639bc5ff
chrisr3d <chris.studer.68@gmail.com>
2019-01-14 15:47:17 +0100

    chg: Added an additional parameter to choose the redis instance(s) to interact with

--- bin/run_redis.py
+++ bin/run_redis.py
@@ -8,15 +8,15 @@ import argparse
 import time
 
 
-def __get_status():
-    backends = {}
-    for b in redis_backends:
+def __get_status(backends):
+    backend_status = {}
+    for b in backends:
         try:
             oui = check_running(b)
-            backends[b] = oui
+            backend_status[b] = oui
         except Exception:
-            backends[b] = False
-    return backends
+            backend_status[b] = False
+    return backend_status
 
 
 def launch_redis(redis_name, storage_directory: Path=None):
@@ -32,27 +32,27 @@ def shutdown_redis(redis_name, storage_directory: Path=None):
     Popen(["./shutdown_redis.sh"], cwd=(storage_directory / redis_name))
 
 
-def check_redis(start, stop):
-    backends = __get_status()
+def check_all(backends, start, stop):
+    backend_status = __get_status(backends)
     if not start and not stop:
-        if all(status for status in backends.values()):
+        if all(status for status in backend_status.values()):
             return "All redis instances runnnig"
-        if not any(status for status in backends.values()):
+        if not any(status for status in backend_status.values()):
             return "All redis instances stopped"
-        return "\n".join(["%s: %s" % (name, "running" if value else "down") for name, value in backends.items()])
+        return "\n".join(["%s: %s" % (name, "running" if value else "down") for name, value in backend_status.items()])
     while True:
-        for b in backends:
+        for b in backend_status:
             try:
-                backends[b] = check_running(b)
+                backend_status[b] = check_running(b)
             except Exception:
-                backends[b] = False
+                backend_status[b] = False
         if stop:
-            if not any(status for status in backends.values()):
+            if not any(status for status in backend_status.values()):
                 return "All redis instances stopped"
         else:
-            if all(status for status in backends.values()):
+            if all(status for status in backend_status.values()):
                 return "All redis instances runnnig"
-        for b, status in backends.items():
+        for b, status in backend_status.items():
             if not stop and not status:
                 print(f"Waiting on {b}")
             if stop and status:
@@ -60,13 +60,13 @@ def check_redis(start, stop):
         time.sleep(1)
 
 
-def launch_all():
-    for b in redis_backends:
+def launch_all(backends):
+    for b in backends:
         launch_redis(b)
 
 
-def shutdown_all():
-    for b in redis_backends:
+def shutdown_all(backends):
+    for b in backends:
         shutdown_redis(b)
 
 
@@ -75,12 +75,14 @@ if __name__ == '__main__':
     parser.add_argument('--start', action='store_true', default=False, help="Start all")
     parser.add_argument('--stop', action='store_true', default=False, help="Stop all")
     parser.add_argument("--status", action='store_true', default=True, help="Show status")
+    parser.add_argument("-n", "--name", type=str, nargs='+', help="Name of the redis instance(s) to interact with. If no name is set, all the currently available instances will be considered.")
     args = parser.parse_args()
+    backends = args.name if args.name is not None else redis_backends
     if args.start and args.stop:
         sys.exit("Please specify if you want either to start or stop redis, but not both.")
     if args.start:
-        launch_all()
+        launch_all(backends)
     elif args.stop:
-        shutdown_all()
+        shutdown_all(backends)
     if args.status:
-        print(check_redis(args.start, args.stop))
+        print(check_all(backends, args.start, args.stop))
